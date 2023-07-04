f8b9e2d04383c6d9428cc88e6250cef005708c17
thomas <hdnhdxv@gmail.com>
2023-07-04 23:43:54 +0400

    ss

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        Person person1 = new Person();
+        person1.name = "sss";
+        person1.age = 11;
+
+        person1.speak();
+    }
+
+        static class  Person{
+        String name;
+        int age;
+        void  speak(){
+            for(int i; i<3; i++){
+            for(int i; i<3; i++){
+                System.out.println(age + name);
+
+            }
+        }
+    }
+
+}
